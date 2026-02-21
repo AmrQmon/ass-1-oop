e56817df8b69bc06bc9bc8edeaa5aea511d47dc4
AmrQmon <heroyt130@gmail.com>
2026-02-22 00:33:13 +0200

    Enhance find_max_array function with comments
    Added comments in Arabic for better understanding.

--- Ass1 OOP.cpp
+++ Ass1 OOP.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 void find_max_array(int n) {
 	int* ptr = new int[n];
-	int max;
+	int max; 
 	cout <<"Enter array's elements\n";
-	for (int g=0 ; g< n ; g++ ) {
+	for (int g=0 ; g< n ; g++ ) { // إدخال المصفوفة
 		cin >>*(ptr+g);
 	}
-    max = *ptr;
+    max = *ptr; // مساواة قيمة ال max بأول عنصر في الآراي
 	for (int i=0 ; i<n ; i++) {
      if (*(ptr + i) > max) {
-    max = *(ptr + i);
+    max = *(ptr + i); // تغيير قيمة max بأكبر قيمة في المصفوفة
 }
  }
 cout << "the max Number in this array is " <<max; 
-delete[] ptr;
+delete[] ptr; // حذف المساحة المأخوذة للآراي 
 }
 int main() {
 	int n;
